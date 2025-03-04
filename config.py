13f9e7e54711e76b9adc13748eb5eb4ec634c2f6
Brinda Bhattarai <brindaabrindaa7@gmail.com>
2025-03-04 14:01:50 +0545

    credentials from env file

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
 
-CLIENT_ID = "5aabba6e9f6844a2a95bcdd3b8f35d56"
-CLIENT_SECRET = "0c4e7238d5ae47a29884863863cef199"
+# Load environment variables from .env file
+load_dotenv()
+
+# credentials from env file
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
